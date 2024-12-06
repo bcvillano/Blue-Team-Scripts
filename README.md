61b5c941cc5a40cfb4606822497576ab493ec70c
bvillano05 <bvillano05@gmail.com>
2024-12-06 16:40:34 -0500

    Added code to get process name and owner for connmon
    Also created pquery file for a program to query information about a process

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ A collection of scripts I've written useful in blue teaming competitions
 
 # Contents
 connmon.py - basically an improved netstat to use to find listening and established connections
+pquery.py - lookup information about processes
  
--- connmon.py
+++ connmon.py
@@ -10,18 +10,21 @@ RESET = "\033[0m"
 def get_listening():
     conns = psutil.net_connections(kind='inet')
     print(f"{RED}Listening Ports{RESET}")
-    print("Proto\tLocal Address\t\t Port\t  Status    PID")
+    print("Proto\tLocal Address\t\t  Port\t   Status    PID\tProcess Name\t     Process Owner")
     for conn in conns:
         if conn.status == psutil.CONN_LISTEN:
             proto = "TCP" if conn.type == socket.SOCK_STREAM else "UDP" if conn.type == socket.SOCK_DGRAM else "UNKNOWN"
             local_ip = conn.laddr.ip if conn.laddr else "*"
             local_port = conn.laddr.port if conn.laddr else "*"
-            print(f"{proto:<8}{local_ip:<25}{local_port:<8} {conn.status:<10}{conn.pid:<10}")
+            process = psutil.Process(conn.pid)
+            process_name = process.name()
+            process_owner = process.username()
+            print(f"{proto:<8}{local_ip:<26}{local_port:<8} {conn.status:<10}{conn.pid:<10} {process_name:<20} {process_owner}")
 
 def get_established():
     conns = psutil.net_connections(kind='inet')
     print(f"\n\n{RED}Established Connections{RESET}")
-    print("Proto\tLocal Address\t\t Local Port\tRemote Address\tRemote Port\t Status        PID")
+    print("Proto\tLocal Address\t\t Local Port\tRemote Address\tRemote Port\t Status        PID\t Process Name\t     Process Owner")
     for conn in conns:
         if conn.status == psutil.CONN_ESTABLISHED:
             proto = "TCP" if conn.type == socket.SOCK_STREAM else "UDP" if conn.type == socket.SOCK_DGRAM else "UNKNOWN"
@@ -29,7 +32,10 @@ def get_established():
             local_port = conn.laddr.port if conn.laddr else "*"
             remote_ip = conn.raddr.ip if conn.raddr else "*"
             remote_port = conn.raddr.port if conn.raddr else "*"
-            print(f"{proto:<8}{local_ip:<25}{local_port:<15}{remote_ip:<16}{remote_port:<17}{conn.status:<14}{conn.pid:<10}")
+            process = psutil.Process(conn.pid)
+            process_name = process.name()
+            process_owner = process.username()
+            print(f"{proto:<8}{local_ip:<25}{local_port:<15}{remote_ip:<16}{remote_port:<17}{conn.status:<14}{conn.pid:<10}{process_name:<20}{process_owner}")
             
     
 
